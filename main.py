376a577b998416b0d58cbd499459b9eb65f2761d
JABorge <60979174+JABorge@users.noreply.github.com>
2022-02-03 13:24:17 -0500

    Recent changes

---
+++ main.py
@@ -0,0 +1,74 @@
+from fastapi import FastAPI, Query, Path, HTTPException, status, Body, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel, Field
+from typing import Optional, List, Dict
+from starlette.responses import HTMLResponse
+from starlette.status import HTTP_400_BAD_REQUEST
+from database import cars
+
+templates = Jinja2Templates(directory="templates")
+
+class Car(BaseModel):
+    make: Optional[str]
+    model: Optional[str]
+    year: Optional[int] = Field(..., ge=1970, lt=2022)
+    price: Optional[float]
+    engine: Optional[str] = "V4"
+    autonomous: Optional[bool]
+    sold: Optional[List[str]]
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request, "title":"FastAPI - Home"})
+
+@app.get("/cars", response_model=List[Dict[str, Car]])
+def get_cars(number: Optional[str] = Query("10", max_length=3)):
+    response = []
+    for id, car in list(cars.items())[:int(number)]:
+        to_add = {}
+        to_add[id] = car 
+        response.append(to_add)
+    return response 
+
+@app.get("/cars/{id}", response_model=Car)
+def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
+    car = cars.get(id)
+    if not car:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Could not find car by ID.")
+    return car
+
+@app.post("/cars", status_code=status.HTTP_201_CREATED)
+def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
+    if len(body_cars) < 1:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,detail="No cars to add.")
+    min_id = len(cars.values()) + min_id
+    for car in body_cars:
+        while cars.get(min_id):
+            min_id += 1
+        cars[min_id] = car 
+        min_id += 1
+
+@app.put("/cars/{id}", response_model=Dict[str,Car])
+def update_car(id: int, car: Car = Body(...)):
+    stored = cars.get(id)
+    if not stored:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Could not find car with given ID")
+    stored = Car(**stored)
+    new = car.dict(exclude_unset=True)
+    new = stored.copy(update=new)
+    cars[id] = jsonable_encoder(new)
+    response = {}
+    response[id] = cars[id]
+    return response
+
+@app.delete("/cars/{id}")
+def delete_cars(id: int):
+    if not cars.get(id):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Could not find car with given ID")
+    del cars[id]
